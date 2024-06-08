32552e043340673a4603dbea5b4a4cf6efeb28e7
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-06-08 04:11:13 -0700

    adding some little changes

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,3 +1,9 @@
-export const GET = () => {
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+
+  // data; format
+
   return new Response("Ok");
 };
